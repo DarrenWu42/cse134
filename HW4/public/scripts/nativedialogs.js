03eba1940c679168345dc6d2d5a98a7182ad160a
dwu <wudarren0908@gmail.com>
2022-02-20 23:03:06 -0800

    hw 4 part 1 finished

--- HW4/public/scripts/nativedialogs.js
+++ HW4/public/scripts/nativedialogs.js
@@ -2,25 +2,34 @@
 
 let $ = (selector) => document.querySelector(selector);
 
-const name = $('#name-input').value;
+let alert_button = $('#alert-button');
+let confirm_button = $('#confirm-button');
+let prompt_button = $('#prompt-button');
+let safer_prompt_button = $('#safer-prompt-button');
 
-const alert_button = $('alert-button');
-const confirm_button = $('confirm-button');
-const prompt_button = $('prompt-button');
-const safer_prompt_button = $('safer-prompt-button');
+let button_output = $('#button-output');
 
-alert_button.addEventListener('click', event => {
-    window.alert();
+let confirm_output = expression => `The value returned by the confirm method is : ${expression}`;
+let prompt_output = expression => `The value returned by the prompt method is : ${expression}`;
+let prompt_negative_output = `User didn't enter anything`;
+
+alert_button.addEventListener('click', () => {
+    window.alert('Alert pressed!');
 });
 
 confirm_button.addEventListener('click', event => {
-    window.confirm();
+    let response = window.confirm('Do you confirm this?');
+    let output = response ? 'OK' : 'Cancel';
+    button_output.innerHTML = confirm_output(output);
 });
 
 prompt_button.addEventListener('click', event => {
-    window.prompt();
+    let response = window.prompt('What is your name?', 'Jim Bob');
+    button_output.innerHTML = prompt_output(response);
 });
 
 safer_prompt_button.addEventListener('click', event => {
-    window.alert();
+    let response = window.prompt('What is your name?', 'Jim Bob');
+    let output = DOMPurify.sanitize(response);
+    button_output.innerHTML = prompt_output(output);
 });
