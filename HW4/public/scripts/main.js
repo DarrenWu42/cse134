c0332e0ce97f613d8722461cb2921ee1d06bb6dc
dwu <wudarren0908@gmail.com>
2022-02-21 03:03:12 -0800

    main js for smooth scrolling

---
+++ HW4/public/scripts/main.js
@@ -0,0 +1,16 @@
+window.addEventListener('DOMContentLoaded', ()=>{
+    const links = document.querySelectorAll("a");
+    
+    for (const link of links) {
+        link.addEventListener("click", clickHandler);
+    }
+    
+    function clickHandler(e) {
+        e.preventDefault();
+        const href = this.getAttribute("href");
+        
+        document.querySelector(href).scrollIntoView({
+            behavior: "smooth"
+        });
+    }
+});
